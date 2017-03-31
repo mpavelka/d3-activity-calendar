e1bbe6ead0bf8b13bb28970ad9bd67cacf6ae373
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2017-03-31 15:51:52 +0200

    fix - providing ‘extend’ function.

--- calendar.js
+++ calendar.js
@@ -1,3 +1,14 @@
+/**
+ * Simplified version of the jQuery $.extend
+ */
+function extend(){
+  for(var i=1; i<arguments.length; i++)
+    for(var key in arguments[i])
+      if(arguments[i].hasOwnProperty(key))
+        arguments[0][key] = arguments[i][key];
+  return arguments[0];
+}
+
 /* Calendar
  * 
  * @param options Calendar options
